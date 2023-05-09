2415f9023ef569926a0cccb7ccde2b5666f7f6bc
Aysur Kuchkashev <aysursabin@gmail.com>
2023-05-09 20:20:54 +0700

    Updates

---
+++ assets/js/index.js
@@ -0,0 +1,1 @@
+(()=>{var e=document.querySelector(".header__menu-btn"),t=document.querySelector(".header__menu");e.addEventListener("click",()=>{e.classList.toggle("active"),t.classList.toggle("active")});})();
